0e5afbc8985315e5f901a43512749639ce8071fb
YeoMinkyu <woas23@naver.com>
2016-12-15 13:26:49 +0900

    sudoku : Add sudoku.h

---
+++ sudoku.h
@@ -0,0 +1,16 @@
+#ifndef SUDOKU_H
+#define SUDOKU_H
+
+#include <stdbool.h>	/* bool */
+
+#define STREAM_LENGTH 82
+
+typedef enum { D_EASY, D_NORMAL, D_HARD } DIFFICULTY;
+
+int		get_holes(DIFFICULTY level);
+char*	difficulty_to_str(DIFFICULTY level);
+char*	generate_puzzle(int holes);
+int		solve(char puzzle[STREAM_LENGTH]);
+bool	is_valid_puzzle(char puzzle[STREAM_LENGTH]);
+
+#endif // SUDOKU_H
